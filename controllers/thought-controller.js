c64aea86d05da31c22a5234fa20094f8ee14fb6e
Chris Key <robertschris010@gmail.com>
2022-10-08 14:04:19 -0500

    looking for err

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -15,5 +15,56 @@ const thoughtControlller = {
             console.log(err);
             res.status(500).json(err);
         });
-    }
+    },
+
+    //get one thoughts by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(400).json({ message: 'No thoughts found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    //add thought to user
+    addThought({ body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'no user found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+
+
+
+
+
+
+
 }
+
+module.exports = thoughtControlller;
