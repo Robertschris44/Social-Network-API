0057caf05dd0c2b4c105860ac2b0cf82de795fcc
Chris Key <robertschris010@gmail.com>
2022-10-08 14:35:44 -0500

    finished thought contr

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -59,12 +59,87 @@ const thoughtControlller = {
         .catch(err => res.json(err));
     },
 
+    //adding reaction to thought
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId},
+            { $push: { reactions: body }},
+            { new: true, runValidators: true }
+        )
+        .populate({ path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'no thoughts found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err =>  res.status(400).json(err)); 
+    },
+
+    //update thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id}, body, {new: true, runValidators: true })
+        .populate({path: 'reactions', select: '-__V'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'no thoughts found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    //remove thought
+    removeThought({params}, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'no thought found'});
+                return;
+            }
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.id } },
+                { new: true}
+            )
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'user not found'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    //remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            {$pull: { reactions: { reactionId: params.reactionId} } },
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'not found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
 
 
 
 
 
 
-}
+
+};
 
 module.exports = thoughtControlller;
